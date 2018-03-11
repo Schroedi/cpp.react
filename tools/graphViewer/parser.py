63a4b6ecdcaf128bbec75046f5fa5f6d55f6f417
Unknown <call-schroedi@web.de>
2018-03-11 22:58:12 +0100

    Timings WIP

--- tools/graphViewer/parser.py
+++ tools/graphViewer/parser.py
@@ -41,16 +41,19 @@ NodeDetach : 100
 > Parent = 3
 """
 
+import numpy as np
 import networkx as nx
 from lark import Lark
 from lark import Transformer, Tree
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+import pydot
 
 
 grammer = r"""
 start: node+
 
-node: "NodeName :" nodeid "=" NODENAME                             -> name
+node: "NodeName :" nodeid "=" NODENAME                          -> name
     | "NodeCreate :" time nodeid nodetype                       -> create
     | "NodeDestroy :" time nodeid                               -> destroy
     | "NodeEvaluateBegin :" time nodeid transactionid threadid  -> eval_begin
@@ -81,37 +84,48 @@ parentid:      "> Parent =" INT
 
 """
 
-# class Node():
-#     def __init__(self, time) -> None:
-#         self.time = time
-#
-#
-# class CreateNode(Node):
-#     def __init__(self, time, id, type) -> None:
-#         super().__init__(time)
-#         self.id = id
-#         self.type = type
-#
-#
-# class DestroyNode(Node):
-#     def __init__(self, time, id) -> None:
-#         super().__init__(time)
-#         self.id = id
 
-
+nodenames = {} # will not be reset, we just need them once
+nodeupdates = {}
+nodetimes = {}
 graph = nx.DiGraph()
-def drawGraph():
-    pos=nx.spring_layout(graph)
-    nx.draw(graph, pos=pos)
-    nx.draw_networkx_labels(graph, pos=pos)
+# node positions in graph
+last_pos = None
+# last time any node did something, used for marking active nodes
+lastActive = 0
+lastCommand = None
+currentTurn = 0
+turnStart = 0
 
-nodenames = {}
+def reset_():
+    global nodeupdates, nodetimes, last_pos, lastActive, lastCommand, currentTurn, turnStart
+    nodeupdates = {}
+    nodetimes = {}
+    graph.clear()
+    last_pos = None
+    lastActive = 0
+    lastCommand = None
+    currentTurn = 0
+    turnStart = 0
+
+reset_()
+
 def get_name(node):
     return nodenames.get(node, node)
 
+
+def updateTurn(time, transactionID):
+    global currentTurn, turnStart
+    if currentTurn < transactionID:
+        turntime = time - turnStart
+        print ("Finished turn {} in {}ms".format(currentTurn, turntime/1000.0))
+        currentTurn = transactionID
+        turnStart = time
+
+
 class PrimitiveReplace(Transformer):
     def name(self, content):
-        nodenames[content[0]] = content[1]
+        nodenames[content[0]] = str(content[1])
         return content
 
     def time(self, n):
@@ -138,7 +152,6 @@ class GraphVisitor(Transformer):
     """
     Transform a parsed tree to a nx graph
     """
-
     def __init__(self, executeCount = None) -> None:
         super().__init__()
         self.currentExec = 0
@@ -146,9 +159,13 @@ class GraphVisitor(Transformer):
 
     def transform(self, tree):
         items = []
+        if self.maxExec is not None and self.currentExec > self.maxExec:
+            return
+
+        global lastCommand
+        lastCommand = tree
+
         for c in tree.children:
-            if self.currentExec > self.maxExec:
-                return
             try:
                 items.append(self.transform(c) if isinstance(c, Tree) else c)
             except Discard:
@@ -162,56 +179,99 @@ class GraphVisitor(Transformer):
             return f(items)
 
     def create(self, content):
-        graph.add_node(get_name(content[1]), node_type=content[2])
-        #drawGraph()
+        global lastActive
+        lastActive = content[0]
+        graph.add_node(get_name(content[1]), node_type=content[2], start_time=content[0], id=content[1])
         return "CREATE: ", content
 
     def destroy(self, content):
-        graph.remove_node(get_name(content[1]))
-        #drawGraph()
+        global lastActive
+        lastActive = content[0]
+        # add deletion time to node
+        graph.add_node(get_name(content[1]), end_time=content[0])
+        #graph.remove_node(get_name(content[1]))
         return "DESTROY: ", content
 
     def eval_begin(self, content):
+        global lastActive
+        updateTurn(content[0], content[2])
+        lastActive = content[0]
+        nodeupdates[content[1]] = nodeupdates.get(content[1], 0) + 1
+        graph.nodes[get_name(content[1])]['laststart'] = content[0]
         return "E_BEGIN: ", content
 
     def eval_end(self, content):
+        global lastActive
+        updateTurn(content[0], content[2])
+        lastActive = content[0]
+        timings = nodetimes.get(content[1], [])
+        starttime = graph.nodes[get_name(content[1])]['laststart']
+        graph.nodes[get_name(content[1])]['lastend'] = content[0]
+        timings.append(content[0] - starttime)
+        nodetimes[content[1]] = timings
+
+        # add stats to node
+        ms_median = np.median(nodetimes[content[1]]) / 1000.0
+        ms_min = np.max(nodetimes[content[1]]) / 1000.0
+        ms_max = np.min(nodetimes[content[1]]) / 1000.0
+        num_evals = nodeupdates[content[1]]
+        graph.add_node(get_name(content[1]),
+                       ms_median=float(ms_median),
+                       ms_min=float(ms_min),
+                       ms_max=float(ms_max),
+                       num_evals=int(num_evals))
+
         return "E_END: ", content
 
     def input_admission(self, content):
+        global lastActive
+        updateTurn(content[0], content[2])
+        lastActive = content[0]
+        graph.nodes[get_name(content[1])]['inputadmission'] = content[0]
         return "INPUT_ADMISSION: ", content
 
     def pulse(self, content):
-        #drawGraph()
+        global lastActive
+        updateTurn(content[0], content[2])
+        lastActive = content[0]
         return "PULSE: ", content
 
     def idle_pulse(self, content):
+        global lastActive
+        updateTurn(content[0], content[2])
+        lastActive = content[0]
         return "PULSE_IDLE: ", content
 
     def attach(self, content):
+        global lastActive
+        lastActive = content[0]
         graph.add_edge(get_name(content[2]), get_name(content[1]))
-        #drawGraph()
         return "ATTACH: ", content
 
     def detach(self, content):
+        global lastActive
+        lastActive = content[0]
         graph.remove_edge(get_name(content[2]), get_name(content[1]))
-        #drawGraph()
         return "DETACH: ", content
 
 l = Lark(grammer)
 #print(l.parse(test_data).pretty())
 #print(GraphVisitor().transform(PrimitiveReplace().transform(l.parse(test_data))))
 
+
+
 commandtree = []
 preparedTree = []
 with open("log.txt") as f:
     commandtree = l.parse(f.read())
     preparedTree = PrimitiveReplace().transform(commandtree)
-    #GraphVisitor().transform(PrimitiveReplace().transform(commandtree))
+    GraphVisitor().transform(preparedTree)
+    nx.write_gexf(graph, "test.gexf")
 
 
 
 
-from matplotlib.widgets import Slider, Button, RadioButtons
+
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.25)
 
@@ -221,22 +281,113 @@ axtime = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 commandCount = len(commandtree.children)
 stime = Slider(axtime, 'Step', 0, commandCount, valinit=0)
 
+def getNodeColor_activity(n):
+    global lastActive
+    start = n[1].get('laststart')
+    end = n[1].get('lastend')
+
+    input = n[1].get('inputadmission')
+    # mark input for 11ms
+    if input is not None and input + 1 * 1000 > lastActive:
+        return 'r'
+
+    if start is None:
+        return 'g'
+    if end is None:
+        return 'y'
+    # was it started more recently than it ended before?
+    return 'y' if end < start else 'g'
+
+
+def getNodeColor_time(n):
+    global nodetimes
+    id = n[1].get('id')
+    return np.log2(np.median(nodetimes.get(id, [0])))
+    #median_ms = [(nid, np.median(times) / 1000.0) for (nid, times) in nodetimes.items()]
+
+def getNodeColor(n):
+    if check.value_selected == 'color: activity':
+        return getNodeColor_activity(n)
+    else:
+        return getNodeColor_time(n)
+
 def update(val):
+    global last_pos
+    global lastCommand
+    reset_()
     t = stime.val
-    graph.clear()
+
+    # evaluate for t steps
     GraphVisitor(executeCount = t).transform(preparedTree)
-    pos=nx.spring_layout(graph)
+    print(lastCommand)
+
+
+
+    #pos = nx.spring_layout(graph, k=0.7, pos=last_pos)
+    pos = nx.circular_layout(graph, dim=2)
+    #pos = nx.nx_pydot.graphviz_layout(graph, prog="osage")
+    last_pos = pos
+
+    # get nodes that currently execute
+    colors = [getNodeColor(n) for n in graph.nodes(data=True)]
+
     ax.cla()
-    nx.draw(graph, pos=pos, ax=ax)
+    # vmin/max
+    nx.draw(graph, pos=pos, ax=ax, node_color=colors)
     nx.draw_networkx_labels(graph, pos=pos, ax=ax)
     fig.canvas.draw_idle()
+
 stime.on_changed(update)
 
 # button
-# resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-# button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975'),
-# def reset(event):
-#     print("btn")
-# button.on_clicked(reset)
+prevax = plt.axes([0.7, 0.025, 0.1, 0.04])
+btnPrev = Button(prevax, 'Prev', color=axcolor, hovercolor='0.975'),
+def previous(event):
+    t = stime.val
+    t = max(0, t - 1)
+    stime.set_val(t)
+
+btnPrev[0].on_clicked(previous)
+
+nextax = plt.axes([0.8, 0.025, 0.1, 0.04])
+btnNext = Button(nextax, 'Next', color=axcolor, hovercolor='0.975'),
+def next(event):
+    t = stime.val
+    t = min(commandCount, t + 1)
+    stime.set_val(t)
+
+btnNext[0].on_clicked(next)
+
+## stats btn
+statax = plt.axes([0.6, 0.025, 0.1, 0.04])
+btnStats = Button(statax, 'Stats', color=axcolor, hovercolor='0.975'),
+def stats(event):
+    # get stats
+    median_ms = [(nid, np.median(times) / 1000.0) for (nid, times) in nodetimes.items()]
+    print([(get_name(nid), time) for (nid, time) in median_ms])
+
+btnStats[0].on_clicked(stats)
+
+# color activity or time
+rax = plt.axes([0.05, 0.4, 0.1, 0.15])
+check = RadioButtons(rax, ('color: activity', 'color: time'))
+def check_fun(label):
+    update(None)
+    print([getNodeColor(n) for n in graph.nodes(data=True)])
+check.on_clicked(check_fun)
+
+
+# ## on click
+# def onclick(e):
+#     global last_pos
+#     #MPL MouseEvent: xy=(549,14) xydata=(0.578125,0.10416666666666674) button=1 dblclick=False inaxes=Axes(0.8,0.025;0.1x0.04)
+#     # get nearest node
+#     ex, ey = e.xdata, e.ydata
+#     dists = [(ex - x)*(ex - x)+(ey - y)*(ey - y) for x,y in last_pos]
+#     closest_id = np.argmin(dists)
+#     print(get_name(closest_id))
+#
+# fig.canvas.mpl_connect('button_press_event', onclick)
+
 
 plt.show()
